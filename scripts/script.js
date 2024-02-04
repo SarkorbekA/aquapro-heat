fafbcb96b547911c94e8cc2b2378157a6f848a11
SARKOR_A <abilkosimovsarkor@gmail.com>
2024-02-04 20:55:13 +0500

    feat: add responsivity

--- scripts/script.js
+++ scripts/script.js
@@ -1,26 +1,29 @@
-// const burgerOpen = document.querySelector('.nav__open');
-// const burgerClose = document.querySelector('.nav__exit-btn');
-// const burgerMenu = document.querySelector('.header-content');
-// const body = document.querySelector('body');
-// const filter = document.querySelector('.filter');
+console.log('Welcome');
 
-// burgerOpen.addEventListener('click', () =>{
-//     burgerMenu.classList.add('active')
-//     body.classList.add('active')
-//     filter.classList.add('active')
-// })
 
-// burgerClose.addEventListener('click', () =>{
-//     burgerMenu.classList.remove('active')
-//     body.classList.remove('active')
-//     filter.classList.remove('active')
-// })
+const burgerOpen = document.querySelector('.nav__open');
+const burgerClose = document.querySelector('.nav__exit-btn');
+const burgerMenu = document.querySelector('.nav__menu');
+const body = document.querySelector('body');
+const filter = document.querySelector('.filter');
 
-// filter.addEventListener('click', () =>{
-//     burgerMenu.classList.remove('active')
-//     body.classList.remove('active')
-//     filter.classList.remove('active')
-// })
+burgerOpen.addEventListener('click', () => {
+    burgerMenu.classList.toggle('active')
+    body.classList.toggle('active')
+    filter.classList.toggle('active')
+})
+
+burgerClose.addEventListener('click', () => {
+    burgerMenu.classList.remove('active')
+    body.classList.remove('active')
+    filter.classList.remove('active')
+})
+
+filter.addEventListener('click', () => {
+    burgerMenu.classList.remove('active')
+    body.classList.remove('active')
+    filter.classList.remove('active')
+})
 
 
 const swiper = new Swiper('.mySwiper', {
@@ -30,6 +33,7 @@ const swiper = new Swiper('.mySwiper', {
         delay: 3000,
         disableOnInteraction: false,
     },
+    spaceBetween: 15,
     navigation: {
         nextEl: '.swiper-button-next1',
         prevEl: '.swiper-button-prev1',
@@ -42,6 +46,24 @@ var swiperCategory = new Swiper(".mySwiper2", {
         nextEl: ".swiper-button-next2",
         prevEl: ".swiper-button-prev2",
     },
-    slidesPerView: 4,
+    slidesPerView: 1,
     spaceBetween: 30,
+    breakpoints: {
+        1101: {
+            slidesPerView: 4,
+            spaceBetween: 30,
+        },
+        993: {
+            slidesPerView: 4,
+            spaceBetween: 20,
+        },
+        769: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+        },
+        481: {
+            slidesPerView: 2,
+            spaceBetween: 15,
+        }
+    },
 });
